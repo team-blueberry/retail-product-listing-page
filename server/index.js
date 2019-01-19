ea7e843c0f0284919e4891ddb2c8545bd989aa5d
Jared Ellison <jaredellison@gmail.com>
2019-01-19 12:32:36 -0500

    Remove unused modules, fill in service endpoints and tidy index.html

--- server/index.js
+++ server/index.js
@@ -1,46 +1,67 @@
+// Dependencies
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const request = require('request');
 const compression = require('compression');
+const morgan = require('morgan');
 const axios = require('axios');
 
-const REVIEW_API_ENDPOINT = 'http://ec2-18-223-135-118.us-east-2.compute.amazonaws.com/listing/';
-const CHECKOUT_API_ENDPOINT = '';
-const CAROUSEL_API_ENDPOINT = '';
+//  Configuration
+const REVIEW_API_ENDPOINT =
+  'http://ec2-18-223-135-118.us-east-2.compute.amazonaws.com/api/reviews/';
+const CHECKOUT_API_ENDPOINT =
+  'http://ec2-18-223-214-235.us-east-2.compute.amazonaws.com/api/checkout/';
+const CAROUSEL_API_ENDPOINT = 'http://34.233.106.94/api/images/';
+const port = process.env.PORT || 3000;
 
+const app = express();
+
+// Middleware
 app.use(compression());
-app.use(express.static(__dirname + '/../public/'));
-app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use('/:id/', express.static(__dirname + '/../public/'));
 
-app.use((req, res, next) => {
- res.header("Access-Control-Allow-Origin", "*");
- res.header(
-   "Access-Control-Allow-Headers",
-   "Origin, X-Requested-With, Content-Type, Accept"
- );
- next();
+// Carousel API
+app.get('/api/images/:id', (req, res) => {
+  let id = req.params.id;
+  axios
+    .get(CAROUSEL_API_ENDPOINT + id)
+    .then(result => {
+      res.send(result.data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
-// TODO: Carousel API
-
-
-// TODO: Checkout API
-
+// Checkout API
+app.get('/api/checkout/:id', (req, res) => {
+  let id = req.params.id;
+  axios
+    .get(CHECKOUT_API_ENDPOINT + id)
+    .then(result => {
+      res.send(result.data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 // Reviews API
-app.get('/listing/:id', (req, res) => {
+app.get('/api/reviews/:id', (req, res) => {
   let id = req.params.id;
-  axios.get(REVIEW_API_ENDPOINT + id).then((result) => {
-    res.send(result.data);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.sendStatus(500);
-  })
-})
+  axios
+    .get(REVIEW_API_ENDPOINT + id)
+    .then(result => {
+      res.send(result.data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
-const port = process.env.PORT || 3000;
+// Start server
 app.listen(port, () => {
   console.log('proxy listening on port 3000');
 });
